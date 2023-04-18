d044ffc69f37f8648d58d0957cdaae85ed5f5932
Ariesta Agung <ariesta.work@gmail.com>
2023-04-19 00:14:03 +0700

    Update folder structure

--- Trinity App/Common/Utils/NetworkManager/NetworkManager.swift
+++ Trinity App/Common/Utils/NetworkManager/NetworkManager.swift
@@ -10,24 +10,20 @@ import Alamofire
 class NetworkManager {
     static let shared: NetworkManager = NetworkManager()
     static let emptyRequest: EmptyRequest = EmptyRequest()
-    func callApi<T: Codable, U: Codable>(urlString: String, parameter: U? = nil, method: HTTPMethod, responseModel: T.Type, completion: @escaping (Result<T, AFError>) -> Void) {
+    func callApi<T: Codable, U: Codable>(urlString: String, parameter: U, method: HTTPMethod, responseModel: T.Type, completion: @escaping (Result<T, AFError>) -> Void) {
         guard let url = URL(string: urlString) else {
             fatalError("Url Not Valid")
         }
         let header: HTTPHeaders = [
             HTTPHeader(name: "Content-type", value: "application/x-www-form-urlencoded")
-            
         ]
         var urlRequest = URLRequest(url: url)
-        if let parameter = parameter {
-            if (parameter is EmptyRequest) == false {
-                let parameterData = try? JSONEncoder().encode(parameter)
-                urlRequest.httpBody = parameterData
-            }
+        if (parameter is EmptyRequest) == false {
+            let parameterData = try? JSONEncoder().encode(parameter)
+            urlRequest.httpBody = parameterData
         }
         urlRequest.headers = header
         urlRequest.method = method
-        
         AF.request(urlRequest)
             .validate()
             .responseDecodable(of: responseModel) { response in
